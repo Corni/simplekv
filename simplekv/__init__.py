690d0eba6802ea5cd9967f7320279eeb48b1349f
Cornelius Riemenschneider <cornelius.riemenschneider@blue-yonder.com>
2017-04-12 14:50:42 +0200

    Allow spaces in keys

--- simplekv/__init__.py
+++ simplekv/__init__.py
@@ -6,10 +6,10 @@ from io import BytesIO
 
 __version__ = '0.10.1.dev1'
 
-VALID_NON_NUM = r"""\`\!"#$%&'()+,-.<=>?@[]^_{}~"""
+VALID_NON_NUM = r"""\`\!"#$%&'()+,-.<=>?@[]^_{}~ """
 VALID_KEY_REGEXP = "^[%s0-9a-zA-Z]+$" % re.escape(VALID_NON_NUM)
 """This regular expression tests if a key is valid. Allowed are all
-alphanumeric characters, as well as ``!"`#$%&'()+,-.<=>?@[]^_{}~``."""
+alphanumeric characters, as well as ``!"`#$%&'()+,-.<=>?@[]^_{}~``. """
 
 VALID_KEY_RE = re.compile(VALID_KEY_REGEXP)
 """A compiled version of :data:`~simplekv.VALID_KEY_REGEXP`."""
--- tests/conftest.py
+++ tests/conftest.py
@@ -32,18 +32,18 @@ def long_value(request):
 
 # keys are always strings. only ascii chars are allowed
 @pytest.fixture(params=[u'short_key', u'different_key',
-                        """'!"`#$%&'()+,-.<=>?@[]^_{}~'"""])
+                        """'!"`#$%&'()+,-.<=>?@[]^_{}~'""", 'some key with space',
+                        'some key with  two consecutive spaces', u'unicode key with spaces'])
 def key(request):
     return request.param
 
 
-@pytest.fixture(params=[u'key_number_2', u'and_again_a_second_key'])
+@pytest.fixture(params=[u'key_number_2', u'and_again_a_second key'])
 def key2(request):
     return request.param
 
 
-@pytest.fixture(params=[u'ä', u'/', u'\x00', u'*', u'',
-                        u'no whitespace allowed'])
+@pytest.fixture(params=[u'ä', u'/', u'\x00', u'*', u''])
 def invalid_key(request):
     return request.param
 
